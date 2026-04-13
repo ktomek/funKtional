051dfc7f80e38b1ffb25c8546bb91ebd20adff4d
Tomasz Kaszkowiak <tomasz.kaszkowiak@gmail.com>
2026-04-13 21:08:20 +0200

    build: add Google Maven repository to plugin management for Android plugin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,13 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
-rootProject.name = "funKtional"
 
+rootProject.name = "funKtional"
